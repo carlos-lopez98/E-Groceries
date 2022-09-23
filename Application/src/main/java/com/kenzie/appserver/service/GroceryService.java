1502d0d163b36e0c94f6693a12973dc144d31364
carlos.lopez98 <cjimenez9811@gmail.com>
2022-09-22 17:40:18 -0700

    test

--- Application/src/main/java/com/kenzie/appserver/service/GroceryService.java
+++ Application/src/main/java/com/kenzie/appserver/service/GroceryService.java
@@ -1,6 +1,5 @@
 package com.kenzie.appserver.service;
 
-<<<<<<< HEAD
 import com.kenzie.appserver.repositories.GroceryRepository;
 import com.kenzie.appserver.repositories.model.GroceryItemRecord;
 import com.kenzie.appserver.service.model.GroceryItem;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GroceryService {
+
     private GroceryRepository groceryRepository;
 
     public GroceryService(GroceryRepository groceryRepository) {
@@ -17,22 +17,22 @@ public class GroceryService {
 
     public GroceryItem addNewItem(GroceryItem item) {
 
-       GroceryItemRecord groceryItemRecord = new GroceryItemRecord();
-       groceryItemRecord.setId(item.getGroceryProductId());
-       groceryItemRecord.setName(item.getGroceryProductName());
-       groceryItemRecord.setDepartment(item.getGroceryProductDepartment());
-       groceryItemRecord.setPrice(item.getGroceryProductPrice());
-       groceryItemRecord.setExpiration(item.getGroceryExpirationDate());
-       groceryItemRecord.setInStock(item.getInStock());
-       groceryItemRecord.setQuantityAvailable(item.getQuantityAvailable());
-       groceryItemRecord.setType(item.getGroceryType());
-       groceryItemRecord.setDiscount(item.getDiscount());
+        GroceryItemRecord groceryItemRecord = new GroceryItemRecord();
+        groceryItemRecord.setId(item.getGroceryProductId());
+        groceryItemRecord.setName(item.getGroceryProductName());
+        groceryItemRecord.setDepartment(item.getGroceryProductDepartment());
+        groceryItemRecord.setPrice(item.getGroceryProductPrice());
+        groceryItemRecord.setExpiration(item.getGroceryExpirationDate());
+        groceryItemRecord.setInStock(item.getInStock());
+        groceryItemRecord.setQuantityAvailable(item.getQuantityAvailable());
+        groceryItemRecord.setType(item.getGroceryType());
+        groceryItemRecord.setDiscount(item.getDiscount());
 
         groceryRepository.save(groceryItemRecord);
 
         return item;
     }
-=======
-public class GroceryService {
->>>>>>> 2d8f250 (Added some file structure)
+
+    public class GroceryService {
+    }
 }
