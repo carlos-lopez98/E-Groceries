09ed544a336abe4eb0dae87853d43586d7c5a5eb
Steven Brazil <stvnbrzl@gmail.com>
2022-09-26 23:17:34 -0500

    Added Method to GroceryService and added unit tests

--- Application/src/main/java/com/kenzie/appserver/service/model/GroceryItem.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/GroceryItem.java
@@ -1,14 +1,10 @@
 package com.kenzie.appserver.service.model;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2d8f250 (Added some file structure)
 import java.util.Date;
 import java.util.Objects;
 
 public class GroceryItem {
-<<<<<<< HEAD
+
     private final String groceryProductId;
     private final String groceryProductName;
     private final String groceryProductDepartment;
@@ -67,37 +63,6 @@ public class GroceryItem {
     public Boolean getDiscount() {
         return discount;
     }
-=======
-    private String groceryProductId;
-    private String groceryProductName;
-    private String groceryProductDepartment;
-    private Double groceryProductPrice;
-    private Date groceryExpirationDate;
-    private String groceryType;
-    private Boolean inStock;
-    private Integer quantityAvailable;
-    private Boolean discount ;
-
-
-    public String getGroceryProductId() {return groceryProductId;}
-    public void setGroceryProductId(String groceryProductId) {this.groceryProductId = groceryProductId;}
-    public String getGroceryProductName() {return groceryProductName;}
-    public void setGroceryProductName(String groceryProductName) {this.groceryProductName = groceryProductName;}
-    public String getGroceryProductDepartment() {return groceryProductDepartment;}
-    public void setGroceryProductDepartment(String groceryProductDepartment) {this.groceryProductDepartment = groceryProductDepartment;}
-    public Double getGroceryProductPrice() {return groceryProductPrice;}
-    public void setGroceryProductPrice(Double groceryProductPrice) {this.groceryProductPrice = groceryProductPrice;}
-    public Date getGroceryExpirationDate() {return groceryExpirationDate;}
-    public void setGroceryExpirationDate(Date groceryExpirationDate) {this.groceryExpirationDate = groceryExpirationDate;}
-    public String getGroceryType() {return groceryType;}
-    public void setGroceryType(String groceryType) {this.groceryType = groceryType;}
-    public Boolean getInStock() {return inStock;}
-    public void setInStock(Boolean inStock) {this.inStock = inStock;}
-    public Integer getQuantityAvailable() {return quantityAvailable;}
-    public void setQuantityAvailable(Integer quantityAvailable) {this.quantityAvailable = quantityAvailable;}
-    public Boolean getDiscount() {return discount;}
-    public void setDiscount(Boolean discount) {this.discount = discount;}
->>>>>>> 2d8f250 (Added some file structure)
 
     @Override
     public boolean equals(Object o){
@@ -111,12 +76,9 @@ public class GroceryItem {
     }
 
     @Override
-<<<<<<< HEAD
+
     public int hashCode(){
         return Objects.hash(groceryProductId, groceryProductName,groceryProductDepartment, groceryType);
     }
-=======
-    public int hashCode(){return Objects.hash(groceryProductId, groceryProductName,groceryProductDepartment, groceryType);}
 
->>>>>>> 2d8f250 (Added some file structure)
 }
