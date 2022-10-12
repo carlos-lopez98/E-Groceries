cbe3a608f9c3eddf7b8566bfa358ea6777c0d487
Steven Brazil <stvnbrzl@gmail.com>
2022-10-11 21:49:29 -0500

    Fix Integration tests and modify updateGroceryItem method

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/QueryUtility.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/QueryUtility.java
@@ -43,7 +43,7 @@ public class QueryUtility {
         }
 
         public ResultActions updateGroceryItem(GroceryItemUpdateRequest groceryItemUpdateRequest) throws Exception{
-            return mvc.perform(put("/grocery-item/")
+            return mvc.perform(post("/grocery-item/")
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(mapper.writeValueAsString(groceryItemUpdateRequest)));
